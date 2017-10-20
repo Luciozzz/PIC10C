867eec10f52f31433388c05e43acd6a7d0a67254
Lucio Zhang <adylucioforzm@live.com>
2017-10-19 22:43:30 -0700

    Modified heading and member functions

--- Assignment1/cards.cpp
+++ Assignment1/cards.cpp
@@ -1,14 +1,9 @@
-
-
-
-
-
 #include "cards.h"
 #include <cstdlib>
 #include <iostream>
 
-/* 
-You might or might not need these two extra libraries 
+/*
+You might or might not need these two extra libraries
 #include <iomanip>
 #include <algorithm>
 */
@@ -18,93 +13,93 @@ You might or might not need these two extra libraries
    Card class
    ************************************************* */
 
-/* 
+/*
    Default constructor for the Card class.
    It could give repeated cards. This is OK.
-   Most variations of Blackjack are played with 
+   Most variations of Blackjack are played with
    several decks of cards at the same time.
 */
 Card::Card(){
    int r = 1 + rand() % 4;
    switch (r) {
       case 1: suit = OROS; break;
-      case 2: suit = COPAS; break; 
+      case 2: suit = COPAS; break;
       case 3: suit = ESPADAS; break;
-      case 4: suit = BASTOS; break; 
+      case 4: suit = BASTOS; break;
       default: break;
    }
 
-   r = 1 + rand() % 10;  
+   r = 1 + rand() % 10;
    switch (r) {
-      case  1: rank = AS; break; 
-      case  2: rank = DOS; break; 
-      case  3: rank = TRES; break; 
-      case  4: rank = CUATRO; break; 
-      case  5: rank = CINCO; break; 
-      case  6: rank = SEIS; break; 
-      case  7: rank = SIETE; break; 
-      case  8: rank = SOTA; break; 
-      case  9: rank = CABALLO; break; 
-      case 10: rank = REY; break; 
+      case  1: rank = AS; break;
+      case  2: rank = DOS; break;
+      case  3: rank = TRES; break;
+      case  4: rank = CUATRO; break;
+      case  5: rank = CINCO; break;
+      case  6: rank = SEIS; break;
+      case  7: rank = SIETE; break;
+      case  8: rank = SOTA; break;
+      case  9: rank = CABALLO; break;
+      case 10: rank = REY; break;
       default: break;
    }
 }
 
-// Accessor: returns a string with the suit of the card in Spanish 
-string Card::get_spanish_suit() const { 
+// Accessor: returns a string with the suit of the card in Spanish
+string Card::get_spanish_suit() const {
    string suitName;
    switch (suit) {
-      case OROS: 
-         suitName = "oros"; 
+      case OROS:
+         suitName = "oros";
 	 break;
-      case COPAS: 
-         suitName = "copas"; 
-	 break; 
-      case ESPADAS: 
-         suitName = "espadas"; 
+      case COPAS:
+         suitName = "copas";
 	 break;
-      case BASTOS: 
-         suitName = "bastos"; 
-	 break; 
+      case ESPADAS:
+         suitName = "espadas";
+	 break;
+      case BASTOS:
+         suitName = "bastos";
+	 break;
       default: break;
    }
    return suitName;
 }
 
-// Accessor: returns a string with the rank of the card in Spanish 
-string Card::get_spanish_rank() const { 
+// Accessor: returns a string with the rank of the card in Spanish
+string Card::get_spanish_rank() const {
    string rankName;
    switch (rank) {
       case AS:
-         rankName = "As"; 
-	 break; 
-      case DOS: 
-         rankName = "Dos"; 
-	 break; 
-      case TRES: 
-         rankName = "Tres"; 
-	 break; 
-      case CUATRO: 
-         rankName = "Cuatro"; 
-	 break; 
-      case CINCO: 
-         rankName = "Cinco"; 
-	 break; 
-      case SEIS: 
-         rankName = "Seis"; 
-	 break; 
-      case SIETE: 
-         rankName = "Siete"; 
-	 break; 
-      case SOTA: 
-         rankName = "Sota"; 
-	 break; 
-      case CABALLO: 
-         rankName = "Caballo"; 
-	 break; 
-      case REY: 
-         rankName = "Rey"; 
-	 break; 
+         rankName = "As";
+	 break;
+      case DOS:
+         rankName = "Dos";
+	 break;
+      case TRES:
+         rankName = "Tres";
+	 break;
+      case CUATRO:
+         rankName = "Cuatro";
+	 break;
+      case CINCO:
+         rankName = "Cinco";
+	 break;
+      case SEIS:
+         rankName = "Seis";
+	 break;
+      case SIETE:
+         rankName = "Siete";
+	 break;
+      case SOTA:
+         rankName = "Sota";
+	 break;
+      case CABALLO:
+         rankName = "Caballo";
+	 break;
+      case REY:
+         rankName = "Rey";
+	 break;
       default: break;
    }
    return rankName;
@@ -112,18 +107,68 @@ string Card::get_spanish_rank() const {
 
 
 
-// Accessor: returns a string with the suit of the card in English 
+// Accessor: returns a string with the suit of the card in English
 // This is just a stub! Modify it to your liking.
-string Card::get_english_suit() const { 
-   return "";
+string Card::get_english_suit() const {
+  string suitName;
+  switch (suit) {
+     case OROS:
+        suitName = "gold";
+  break;
+     case COPAS:
+        suitName = "trophy";
+  break;
+     case ESPADAS:
+        suitName = "matador";
+  break;
+     case BASTOS:
+        suitName = "bullet";
+  break;
+     default: break;
+  }
+  return suitName;
+  ;
 }
 
-// Accessor: returns a string with the rank of the card in English 
+// Accessor: returns a string with the rank of the card in English
 // This is just a stub! Modify it to your liking.
-string Card::get_english_rank() const { 
-   return "";
+string Card::get_english_rank() const {
+  string rankName;
+  switch (rank) {
+     case AS:
+        rankName = "One";
+  break;
+     case DOS:
+        rankName = "Two";
+  break;
+     case TRES:
+        rankName = "Three";
+  break;
+     case CUATRO:
+        rankName = "Four";
+  break;
+     case CINCO:
+        rankName = "Five";
+  break;
+     case SEIS:
+        rankName = "Six";
+  break;
+     case SIETE:
+        rankName = "Seven";
+  break;
+     case SOTA:
+        rankName = "Jack";
+  break;
+     case CABALLO:
+        rankName = "Queen";
+  break;
+     case REY:
+        rankName = "King";
+  break;
+     default: break;
+  }
+  return rankName;
 }
-
 
 
 // Assigns a numerical value to card based on rank.
@@ -151,4 +196,3 @@ bool Card::operator < (Card card2) const {
    Player class
    ************************************************* */
 // Implemente the member functions of the Player class here.
-
--- Assignment1/cards.h
+++ Assignment1/cards.h
@@ -16,39 +16,39 @@ using namespace std;
 enum suit_t {OROS, COPAS, ESPADAS, BASTOS};
 
    /*
-   The values for this type start at 0 and increase by one 
-   afterwards until they get to SIETE. 
-   The rank reported by the function Card::get_rank() below is 
+   The values for this type start at 0 and increase by one
+   afterwards until they get to SIETE.
+   The rank reported by the function Card::get_rank() below is
    the value listed here plus one.
-   E.g: 
+   E.g:
    The rank of AS is reported as    static_cast<int>(AS) + 1   = 0 + 1 =  1
    The rank of SOTA is reported as  static_cast<int>(SOTA) + 1 = 9 + 1 = 10
    */
 enum rank_t {AS, DOS, TRES, CUATRO, CINCO, SEIS, SIETE, SOTA=9, CABALLO=10, REY=11};
 
 class Card {
-   public: 
+   public:
       // Constructor assigns random rank & suit to card.
       Card();
-      
-      // Accessors 
-      string get_spanish_suit() const;
-      string get_spanish_rank() const; 
 
-      /* 
-         These are the only functions you'll need to code 
-         for this class. See the implementations of the two 
+      // Accessors
+      string get_spanish_suit() const;
+      string get_spanish_rank() const;
+
+      /*
+         These are the only functions you'll need to code
+         for this class. See the implementations of the two
 	 functions above to get an idea of how to proceed.
       */
       string get_english_suit() const;
-      string get_english_rank() const; 
+      string get_english_rank() const;
 
       // Converts card rank to number.
       // The possible returns are: 1, 2, 3, 4, 5, 6, 7, 10, 11 and 12
       int get_rank() const;
 
       // Compare rank of two cards. E.g: Eight<Jack is true.
-      // Assume Ace is always 1. 
+      // Assume Ace is always 1.
       // Useful if you want to sort the cards.
       bool operator < (Card card2) const;
 
@@ -67,12 +67,13 @@ class Hand {
 
    private:
       // You decide what fields you'll need...
+      vector<Card> v;
 };
 
 
 class Player {
    public:
-      // Constructor. 
+      // Constructor.
       //    Assigns initial amount of money
       Player(int m);
 
