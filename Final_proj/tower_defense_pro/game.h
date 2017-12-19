0d849fdd01712cd7806dd78e8314e1f7af7973a6
Lucio Zhang <adylucioforzm@live.com>
2017-12-19 01:45:45 -0800

    better version of the game

---
+++ Final_proj/tower_defense_pro/game.h
@@ -0,0 +1,27 @@
+#ifndef GAME_H
+#define GAME_H
+
+#include <QGraphicsView>
+#include <QMouseEvent>
+#include <QKeyEvent>
+#include <QVector>
+
+#include "Tower.h"
+#include "Enemy.h"
+
+class Game: public QGraphicsView{
+public:
+    Game(int);
+
+    void mousePressEvent(QMouseEvent* event);
+    void keyPressEvent(QKeyEvent *key);
+
+    QGraphicsScene* scene;
+    Tower* t;
+    QPoint* center;
+    QVector<Enemy*> enemy_list;
+    const QVector<int> spawn_pos = {0,80,160,240,320,400,480,560,640,720};
+    QVector<double> speed_list;
+};
+
+#endif // GAME_H
